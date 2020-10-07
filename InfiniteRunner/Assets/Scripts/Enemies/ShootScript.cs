fee1ad63b0860f2274460e087ee23c1d47b26c70
Maxfield Barden <mbarden8@vt.edu>
2020-10-07 02:06:19 -0400

    added bullet model, fixed bugs, enemies destroy themselves

--- InfiniteRunner/Assets/Scripts/Enemies/ShootScript.cs
+++ InfiniteRunner/Assets/Scripts/Enemies/ShootScript.cs
@@ -18,6 +18,10 @@ public class ShootScript : MonoBehaviour
     private Transform head;
     [SerializeField]
     private Transform legs;
+    //[SerializeField]
+    private float walkSpeed = 0.5f;
+    private float removalDistance = 10;
+    Animator _anim;
 
     private float _torsoMoveSpeed;
     private float _headMoveSpeed;
@@ -44,9 +48,11 @@ public class ShootScript : MonoBehaviour
             Debug.Log("Did not assign legs in hierarchy");
 
 
-        _moveBackwardDistance = Random.Range(15, 20);
+        _moveBackwardDistance = Random.Range(50, 100);
         _rb = this.GetComponent<Rigidbody>();
+        _anim = this.GetComponentInChildren<Animator>();
     }
+    
     private void FixedUpdate()
     {
 
@@ -54,26 +60,32 @@ public class ShootScript : MonoBehaviour
         MoveBackwards();
         if(Time.time%Random.Range(3,5)==0)
             Shoot();
+        destroyModel();
     }
     private void LookAtTarget()
     {
         Vector3 _lookDirection = objectToFollow.position - transform.position;
+        _lookDirection.y = 0;
         Quaternion _rot = Quaternion.LookRotation(_lookDirection, Vector3.up);
+
 
 
         torso.transform.rotation = Quaternion.Lerp(transform.rotation, _rot, _torsoMoveSpeed * Time.deltaTime);
         head.transform.rotation = Quaternion.Lerp(transform.rotation, _rot, _headMoveSpeed * Time.deltaTime);
         legs.transform.rotation = Quaternion.Lerp(transform.rotation, _rot, _legMoveSpeed * Time.deltaTime);
-       
+   
+
     }
     private void MoveBackwards()
     {
         float positionDifference = transform.position.x - objectToFollow.position.x;
         if (positionDifference < _moveBackwardDistance && positionDifference>0)
         {
-            _rb.velocity = new Vector3(transform.forward.x * -3f, 0, transform.forward.z * -(1f/positionDifference*2));
+            _rb.velocity = new Vector3(transform.forward.x * -walkSpeed, 0, transform.forward.z * -(1f/positionDifference*2));
+            _anim.SetTrigger("PlayWalkBackward");
         }
     }
+
     private void Shoot()
     {
         Vector3 _pointToAimAt = new Vector3(objectToFollow.position.x, objectToFollow.position.y, objectToFollow.position.z);
@@ -85,6 +97,15 @@ public class ShootScript : MonoBehaviour
         Vector3 spawnPos = torso.transform.position + torso.transform.forward*1.1f;
         Instantiate(bullet, spawnPos, _lookRotation); 
 
+    }
+
+    private void destroyModel()
+    {
+        float positionDifference = objectToFollow.position.x - transform.position.x;
+        if (positionDifference > removalDistance)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
 }
