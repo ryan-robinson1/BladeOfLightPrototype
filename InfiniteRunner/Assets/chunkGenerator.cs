ca3f8b83be24d6a676d204ffff33bd91fd90cde5
ryan-robinson1 <69409000+ryan-robinson1@users.noreply.github.com>
2020-10-26 01:04:26 -0400

    Simple Infinite Chunk System

---
+++ InfiniteRunner/Assets/chunkGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chunkGenerator : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject[] chunks;
+    [SerializeField]
+    private float chunkSize = 300;
+    [SerializeField]
+    private GameObject player;
+
+    private int relativePosition;
+    private int distanceTraveled;
+
+    private int lastPositionSpawned = 150;
+    private Queue<GameObject> renderedObjects = new Queue<GameObject>();
+  
+    private void Update()
+    {
+        Debug.Log(relativePosition);
+        if (relativePosition >= chunkSize)
+        {
+            relativePosition = 0;
+            distanceTraveled = Mathf.RoundToInt(player.transform.position.x);
+        }
+        else
+        {
+            relativePosition = Mathf.RoundToInt(player.transform.position.x)-distanceTraveled;
+        }
+        if (relativePosition == 150)
+
+        {
+            int nextSpawnPosition = distanceTraveled + Mathf.RoundToInt(relativePosition) + 300;
+            if (lastPositionSpawned != nextSpawnPosition)
+            {
+                renderedObjects.Enqueue(Instantiate(chunks[Random.Range(0, 3)], new Vector3(nextSpawnPosition, 0, 0), Quaternion.Euler(0, 0, 0)));
+
+                if (renderedObjects.Count > 2)
+                {
+                    Destroy(renderedObjects.Dequeue());
+                }
+            }
+
+            lastPositionSpawned = nextSpawnPosition;
+        }
+    }
+
+}
