722f7dfdd1dcf761215e919241afc5edb6b48647
ryan-robinson1 <69409000+ryan-robinson1@users.noreply.github.com>
2020-12-15 17:39:25 -0500

    Started Menu UI

--- InfiniteRunner/Assets/Scripts/StartMenuScript.cs
+++ InfiniteRunner/Assets/Scripts/StartMenuScript.cs
@@ -6,8 +6,9 @@ public class StartMenuScript : MonoBehaviour
 {
     public GameObject player;
     public GameObject camera;
+    public GameObject UI;
     PlayerAnimationController _animController;
-
+    public bool started = false;
     private void Start()
     {
         //StartGame();
@@ -20,11 +21,20 @@ public class StartMenuScript : MonoBehaviour
         player.transform.rotation = Quaternion.Euler(0, 0, 0);
         camera.transform.rotation = Quaternion.Euler(28f, 90, 0);
         _animController = player.GetComponent<PlayerAnimationController>();
+        UI.GetComponent<Canvas>().enabled = true;
 
         player.GetComponent<Rigidbody>().transform.position = new Vector3(0, 1.15f, 0);
         player.GetComponent<PlayerController>().enabled = true;
         _animController.StartGame();
         camera.GetComponent<CameraFollowScript>().enabled = true;
         this.GetComponentInChildren<Canvas>().enabled = false;
+        started = true;
+    }
+    private void Update()
+    {
+        if (SwipeInput.Instance.Tap && !started)
+        {
+            StartGame();
+        }
     }
 }
