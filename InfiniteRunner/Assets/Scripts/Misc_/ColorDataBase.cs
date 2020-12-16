936963659f8c7ae9281235a6c6eaa3b39d3f3133
Maxfield Barden <mbarden8@vt.edu>
2020-12-16 15:39:07 -0500

    minor changes

--- InfiniteRunner/Assets/Scripts/Misc_/ColorDataBase.cs
+++ InfiniteRunner/Assets/Scripts/Misc_/ColorDataBase.cs
@@ -52,9 +52,9 @@ public static class ColorDataBase
         };
 
     // will need to change this later to make it more variable
-    private static Color heroColor = heroColors["hero-pink"];
+    private static Color heroColor = heroColors["hero-blue"];
 
-    private static Color swordColor = swordColors["sword-pink"];
+    private static Color swordColor = swordColors["sword-blue"];
 
 
     /**
