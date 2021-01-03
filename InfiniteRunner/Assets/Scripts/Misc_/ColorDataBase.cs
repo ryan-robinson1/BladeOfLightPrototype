3322cbdcc89289e889cd3d45875ed9ec62b09317
ryan-robinson1 <69409000+ryan-robinson1@users.noreply.github.com>
2021-01-03 17:32:02 -0500

    Greeen

--- InfiniteRunner/Assets/Scripts/Misc_/ColorDataBase.cs
+++ InfiniteRunner/Assets/Scripts/Misc_/ColorDataBase.cs
@@ -108,12 +108,12 @@ public static class ColorDataBase
 
 
     // will need to change this later to make it more variable
-    private static Color heroColor = heroColors["hero-blue"];
+    private static Color heroColor = heroColors["hero-green"];
 
-    private static Color enemyMain = enemyColors["enemyDissolve-green"];
-    private static Color enemyStrip = enemyColors["enemyStrip-green"];
+    private static Color enemyMain = enemyColors["enemyDissolve-red"];
+    private static Color enemyStrip = enemyColors["enemyStrip-red"];
 
-    private static Color swordColor = swordColors["sword-red"];
+    private static Color swordColor = swordColors["sword-green"];
 
 
     /**
