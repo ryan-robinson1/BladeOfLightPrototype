244577574603bd34f1ef8fa6fe4a73c90ddf6264
ryan-robinson1 <69409000+ryan-robinson1@users.noreply.github.com>
2021-01-04 19:19:20 -0500

    enemySpawnScript

--- InfiniteRunner/Assets/Scripts/chunkGenerator/ChunkGenerator.cs
+++ InfiniteRunner/Assets/Scripts/chunkGenerator/ChunkGenerator.cs
@@ -30,6 +30,7 @@ public class ChunkGenerator : MonoBehaviour
     float currentSpawnXRight = 0;
     float streetLightSpawnX = 0;
     float roadPatchX = 0;
+    float enemiesX = 0;
     private ArrayList enemySpawnPoints = new ArrayList();
     private void Start()
     {
@@ -102,28 +103,45 @@ public class ChunkGenerator : MonoBehaviour
     }
     void spawnEnemies()
     {
+     
 
-    /*    float[] possibleZCoor = { -3.5f, -1.75f, 0, 1.75f, 3.5f };
-        int xCoor = Random.Range(0, 18) * 10;
-        *//*while (enemySpawnPoints.Contains(xCoor))
+        for(int i = 0; i < Random.Range(5, 7);i++)
         {
-            xCoor = Random.Range(0, 30) * 10;
+            Instantiate(enemy,new Vector3(enemiesX,0,0)+returnEnemySpawnPoint(), Quaternion.Euler(0, -90, 0));
         }
-        *//*
-        enemySpawnPoints.Add(xCoor);
+        enemiesX += 150;
+        
 
+        /*    float[] possibleZCoor = { -3.5f, -1.75f, 0, 1.75f, 3.5f };
+            int xCoor = Random.Range(0, 18) * 10;
+            *//*while (enemySpawnPoints.Contains(xCoor))
+            {
+                xCoor = Random.Range(0, 30) * 10;
+            }
+            *//*
+            enemySpawnPoints.Add(xCoor);
+
+            float zCoor = possibleZCoor[Random.Range(0, 5)];
+
+            if (Random.Range(0, 25) == 1)
+            {
+                Instantiate(healthPack, new Vector3(Mathf.RoundToInt(player.transform.position.x) + 90 + xCoor, 0.933f, zCoor), Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(enemy, new Vector3(Mathf.RoundToInt(player.transform.position.x) + 90 + xCoor, 0.72f, zCoor), Quaternion.Euler(0, -90, 0));
+            }*/
+
+
+
+    }
+    private Vector3 returnEnemySpawnPoint()
+    {
+        float[] possibleZCoor = { -3.5f, -1.75f, 0, 1.75f, 3.5f };
         float zCoor = possibleZCoor[Random.Range(0, 5)];
-
-        if (Random.Range(0, 25) == 1)
-        {
-            Instantiate(healthPack, new Vector3(Mathf.RoundToInt(player.transform.position.x) + 90 + xCoor, 0.933f, zCoor), Quaternion.identity);
-        }
-        else
-        {
-            Instantiate(enemy, new Vector3(Mathf.RoundToInt(player.transform.position.x) + 90 + xCoor, 0.72f, zCoor), Quaternion.Euler(0, -90, 0));
-        }*/
-
-
+        float xCoor = Random.Range(0, 180);
+        float yCoor = 0.43f;
+        return new Vector3(xCoor, yCoor, zCoor);
 
     }
 }
