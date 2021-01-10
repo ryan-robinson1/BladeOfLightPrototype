a27aada3b563339c4afec4c74e77b89b59d2a7c2
ryan-robinson1 <69409000+ryan-robinson1@users.noreply.github.com>
2021-01-10 17:07:47 -0500

    Fromula2

--- InfiniteRunner/Assets/Scripts/GameStates/GameOverState.cs
+++ InfiniteRunner/Assets/Scripts/GameStates/GameOverState.cs
@@ -117,9 +117,10 @@ public class GameOverState : MonoBehaviour
     */
     public void calculateBounty()
     {
-        float g = ((score / 1000) + streak) / 100;
-        float p = Mathf.Pow(g, 1f / 3f) * 535;
-        bounty = Mathf.RoundToInt(p); 
+        double g = (double)(((double)((double)score / 1000.0) + (double)streak)/100.0);
+        double p = System.Math.Pow(g,(1.0/3.0))*175.0;
+        Debug.Log(p);
+        bounty = (int)p;
 
         shop.GetComponent<Shop>().depositMoney(bounty);
     }
