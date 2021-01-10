e3b5249751594c8385afe7f588c58661a6719133
ryan-robinson1 <69409000+ryan-robinson1@users.noreply.github.com>
2021-01-10 17:55:21 -0500

    More Shop Stuff

--- InfiniteRunner/Assets/Scripts/ShopScripts/ShopInterfaceNav.cs
+++ InfiniteRunner/Assets/Scripts/ShopScripts/ShopInterfaceNav.cs
@@ -6,15 +6,22 @@ public class ShopInterfaceNav : MonoBehaviour
 {
     public Camera ShopCamera;
     public Camera MainMenuCamera;
-    public Canvas ShopCanvas;
+    public Canvas PlayerShopCanvas;
+    public Canvas EnemyShopCanvas;
+    public Canvas SwordShopCanvas;
     public Canvas mainMenuCanvas;
     public GameObject shop;
-   
+    bool moveToEnemyShop = false;
+    bool moveToPlayerShop = false;
+    bool moveToSwordShop = false;
+    Vector3 enemyShopCameraPosition = new Vector3(-8.152f, 2.43f, 10.6f);
+    Vector3 playerShopCameraPosition = new Vector3(-15f, 2.43f, 10.6f);
+    Vector3 swordShopCameraPosition = new Vector3(-21.848f, 2.43f, 10.6f);
     public void goToShop()
     {
         ShopCamera.enabled = true;
         MainMenuCamera.enabled = false;
-        ShopCanvas.enabled = true;
+        PlayerShopCanvas.enabled = true;
         mainMenuCanvas.enabled = false;
         shop.transform.GetChild(0).gameObject.SetActive(true);
     }
@@ -22,8 +29,58 @@ public class ShopInterfaceNav : MonoBehaviour
     {
         ShopCamera.enabled = false;
         MainMenuCamera.enabled = true;
-        ShopCanvas.enabled = false;
+        PlayerShopCanvas.enabled = false;
         mainMenuCanvas.enabled = true;
         shop.transform.GetChild(0).gameObject.SetActive(false);
+    }
+    public void goToEnemyShop()
+    {
+        moveToEnemyShop = true;
+       
+        PlayerShopCanvas.enabled = false;
+    }
+    public void goToPlayerShop()
+    {
+        EnemyShopCanvas.enabled = false;
+        SwordShopCanvas.enabled = false;
+       
+        moveToPlayerShop = true;
+    }
+    public void goToSwordShop()
+    {
+        PlayerShopCanvas.enabled = false;
+        
+        moveToSwordShop = true;
+
+    }
+    void Update()
+    {
+        if (moveToEnemyShop)
+        {
+            ShopCamera.transform.position = Vector3.MoveTowards(ShopCamera.transform.position, enemyShopCameraPosition, Time.deltaTime*10);
+            if(ShopCamera.transform.position == enemyShopCameraPosition)
+            {
+                moveToEnemyShop = false;
+                EnemyShopCanvas.enabled = true;
+            }
+        }
+        else if (moveToPlayerShop)
+        {
+            ShopCamera.transform.position = Vector3.MoveTowards(ShopCamera.transform.position, playerShopCameraPosition, Time.deltaTime * 10);
+            if (ShopCamera.transform.position == playerShopCameraPosition)
+            {
+                moveToPlayerShop = false;
+                PlayerShopCanvas.enabled = true;
+            }
+        }
+        else if (moveToSwordShop)
+        {
+            ShopCamera.transform.position = Vector3.MoveTowards(ShopCamera.transform.position, swordShopCameraPosition, Time.deltaTime * 10);
+            if (ShopCamera.transform.position == swordShopCameraPosition)
+            {
+                moveToSwordShop = false;
+                SwordShopCanvas.enabled = true;
+            }
+        }
     }
 }
