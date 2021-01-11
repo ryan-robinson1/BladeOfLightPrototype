3abe46a1d10a240343c75992250e427f482902e9
ryan-robinson1 <69409000+ryan-robinson1@users.noreply.github.com>
2021-01-11 15:09:04 -0500

    Revert "Revert "Added Shop Functionality""
    This reverts commit 49dc7f2870def2b4ecf36f4c04a7319f7c0609e0.

--- InfiniteRunner/Assets/Scripts/ShopScripts/Item.cs
+++ InfiniteRunner/Assets/Scripts/ShopScripts/Item.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Item 
+public class Item : MonoBehaviour
 {
     public GameObject prefab;
     public string name;
     public int price;
+    public bool defaultItem;
+    public string color;
+    //Change to class later
+    public string achievement;
 
-    [HideInInspector]
-    public bool purchased;
-    public bool equipped;
+    public ButtonState purchaseState;
+
+    public enum ButtonState
+    {
+        purchased,
+        equipped,
+        unlocked,
+        locked
+    }
 }
 
