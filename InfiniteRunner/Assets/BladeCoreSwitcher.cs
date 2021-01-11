9a4df791016c7ade6b0f0f35110f889bf1f53931
ryan-robinson1 <69409000+ryan-robinson1@users.noreply.github.com>
2021-01-10 20:52:45 -0500

    Sword Switch

---
+++ InfiniteRunner/Assets/BladeCoreSwitcher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BladeCoreSwitcher : MonoBehaviour
+{
+ 
+    public GameObject coreSelect;
+    public GameObject swordSelect;
+    public Text buttonText;
+    bool blade = false;
+
+    public void switchSelects()
+    {
+        if (blade)
+        {
+            coreSelect.SetActive(true);
+            swordSelect.SetActive(false);
+            blade = false;
+            buttonText.text = "Cores";
+        }
+        else
+        {
+            coreSelect.SetActive(false);
+            swordSelect.SetActive(true);
+            blade = true;
+            buttonText.text = "Blades";
+        }
+    }
+}
