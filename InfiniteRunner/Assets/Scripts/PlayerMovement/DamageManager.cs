c64e33dba4c83d9a08218d117fd33bb90eb6794f
Maxfield Barden <mbarden8@vt.edu>
2021-01-13 15:20:08 -0500

    fixed hero mat glitch

--- InfiniteRunner/Assets/Scripts/PlayerMovement/DamageManager.cs
+++ InfiniteRunner/Assets/Scripts/PlayerMovement/DamageManager.cs
@@ -41,11 +41,7 @@ public class DamageManager : MonoBehaviour
         edgeWidth = healthIndicator.GetFloat("edgeWidth");
         SetHeroColor();
 
-        lightfxModule = damageLightEffect.main;
-        lightfxModule.startColor = ColorDataBase.GetHeroAlbedo();
-
-        healfxModule = healLightFX.main;
-        healfxModule.startColor = ColorDataBase.GetHeroAlbedo();
+        
 
         gameOverState = this.GetComponentInChildren<GameOverState>();
     }
@@ -62,6 +58,11 @@ public class DamageManager : MonoBehaviour
     {
         healthIndicator.SetColor("dissolveColor",
            ColorDataBase.GetCurrentHeroColor());
+        lightfxModule = damageLightEffect.main;
+        lightfxModule.startColor = ColorDataBase.GetHeroAlbedo();
+
+        healfxModule = healLightFX.main;
+        healfxModule.startColor = ColorDataBase.GetHeroAlbedo();
     }
     /**
      * Displays information on screen.
